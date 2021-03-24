0fa2ab26c830638a1865c823ed13442b9a588483
Peehii <a180616@gmail.com>
2021-03-24 22:34:42 +0800

    Java and Kotlin variable, data type.

--- src/main/java/com/fly/Hello.java
+++ src/main/java/com/fly/Hello.java
@@ -6,5 +6,14 @@ public class Hello {
 //        new Person().Hello();
         Person p = new Person();
         p.Hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;   //預設為double，需改為float
+        boolean adult = true;
+        String name = "Fly";
+
     }
 }
